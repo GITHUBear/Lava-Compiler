729d43df0255220a61413da535c424774c0a3cfd
tomokazu <1729307751@qq.com>
2019-05-13 23:44:32 +0800

    part1: lex start

---
+++ wordunit.h
@@ -0,0 +1,15 @@
+#ifndef WORD_UNIT_H
+#define WORD_UNIT_H
+
+// reserved word
+#define BOOL 1
+#define BREAK 2
+#define CHAR 3
+#define CONTINUE 4
+#define DO 5
+#define ELSE 6
+#define FALSE 7
+#define FLOAT 8
+#define FOR 9
+#define IF 10
+#define INT 11
